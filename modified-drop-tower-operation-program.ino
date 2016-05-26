e93befe72024a4b455c663588e11d26758056919
kaustav927 <Kaustav@Kaustavs-MacBook-Pro.local>
2016-05-26 19:16:52 -0400

    Added some stepper motor capabilities  along with a button to control it. Also cleaned up some code.

--- modified-drop-tower-operation-program.ino
+++ modified-drop-tower-operation-program.ino
@@ -7,7 +7,7 @@ makes stepper motor go 2 directions with 2 push buttons and has variable speed
 on the second direction 
 
 Author: Kaustav Sharma
-
+DAte
 */
 
 
@@ -16,25 +16,25 @@ Author: Kaustav Sharma
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 #include <Servo.h> 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
-Adafruit_StepperMotor *myStepper = AFMS.getStepper(200, 2);
+Adafruit_StepperMotor *myMotor = AFMS.getStepper(200, 2);
 
 
  // Set digital pin numbers:
  const int servoPin1 = 9;  // The first servo pin 
  const int servoPin2 = 10; // The second servo pin 
- const int buttonPin = 11;  // The push button that changes the servo direstion 
+ const int servoButtonPin = 11;  // The push button that changes the servo direstion 
+ const int stepperButtonPin=2;
 
+ int sPeed =25; // delay between steps (low=fast)
+ int fullRev=200; //the number of steps that make a full rotation
+ int distance=0;
 
 //BUTTONS 
  int button1State = 0;  // Variable for reading the pushbutton status
- int direction1State = 0;  // Variable for reading direction of the servo
+ int directionState = 0;  // Variable for reading direction of the servo
 
- int button2State=0;
- int direction2State=0;
- 
- int button3State=0;
- int direction3State=0; 
-
+int button2State =0;
+int OnOff=0;
 
 
 
@@ -44,19 +44,21 @@ Adafruit_StepperMotor *myStepper = AFMS.getStepper(200, 2);
 
 
  void setup() {
+ pinMode (stepperButtonPin,INPUT); // initialize the stepper pushbutton pin as an input
+ 
 
   
-  servo1.attach(9); // attaches the servo on pin 8 to the servo object 
-  servo2.attach(10);
+  servo1.attach(9); // attaches the servo on pin 9 to the servo object 
+  servo2.attach(10); // attaches the servo on pin 10 to the servo object 
+  pinMode(servoButtonPin, INPUT);  // initialize the servo pushbutton pin as an input
+  
 
   //LED TURNS ON
   pinMode(13, OUTPUT);
-  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-    
-
-
-   pinMode(buttonPin, INPUT);  // initialize the pushbutton pin as an input
- }b b
+  digitalWrite(13, HIGH);   // turns the LED on 
+   
+   
+ }
 
  void loop(){
 
@@ -69,11 +71,11 @@ Adafruit_StepperMotor *myStepper = AFMS.getStepper(200, 2);
    */
   
    // read the state of the pushbutton value:
-   buttonState = digitalRead(buttonPin);
+   button1State = digitalRead(servoButtonPin);
 
    if (directionState == 0){
      //The button is pushed
-     if (buttonState == HIGH) {
+     if (button1State == HIGH) {
        directionState = 1;// The direction for the servo is clockwise
 
     // goes from 0 degrees to 180 degrees in steps of 1 degree
@@ -87,7 +89,7 @@ Adafruit_StepperMotor *myStepper = AFMS.getStepper(200, 2);
 
    } else if (directionState == 1) {
      // The button is pushed
-     if (buttonState == HIGH) {
+     if (button1State == HIGH) {
       directionState = 0;  // The direction for the servo is anti-clockwise 
 
        // goes from 180 degrees to 0 degrees in steps of 1 degree 
@@ -99,19 +101,30 @@ Adafruit_StepperMotor *myStepper = AFMS.getStepper(200, 2);
        }
      }
    }
- /*
-   * 
-   * SERVO PART 
-   * 
-   * 
-   * 
-   */
 
+/*
+ * STEPPER PART
+ * 
+ * 
+ */
 
+ myMotor->setSpeed(100);  // 10 rpm   
+ 
+ button2State= digitalRead(stepperButtonPin);
+if (button2State == LOW) 
+{
+  OnOff=1; //should run when it is 1
+}
 
+while (OnOff ==1)
+{
+   myMotor->step(100, FORWARD, SINGLE); 
+   distance= distance + 1;
 
-
-
+   if (distance == fullRev){
+    OnOff=0;
+   }
+}
 
 
 
