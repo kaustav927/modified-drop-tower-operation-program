6b7cd7e327b4384c86ef46bc8d7b56b091577538
kaustav927 <Kaustav@Kaustavs-MacBook-Pro.local>
2016-06-14 12:42:40 -0400

    added stepper motor capabilities
    Stepper motor now work with AFM shield. Button on pin 2 is functioning
    for stepper motor.

--- modified-drop-tower-operation-program.ino
+++ modified-drop-tower-operation-program.ino
@@ -1,82 +1,73 @@
 /*
-
-project: drop tower ride modifications 
-
-Description: turns servos in alternating directions with 1 push button, and 
-makes stepper motor go 2 directions with 2 push buttons and has variable speed 
-on the second direction 
-
 Author: Kaustav Sharma
-DAte
+
 */
 
+//objects
+#include <Adafruit_MotorShield.h>
 
 #include <Wire.h>
-#include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 #include <Servo.h> 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+//Adafruit_StepperMotor *myStepper = AFMS.getStepper(200, 2);
 Adafruit_StepperMotor *myMotor = AFMS.getStepper(200, 2);
+
+//Servo objects
+
+Servo servo1;// First servo object 
+Servo servo2; // second servo object 
+int pos = 0;  // Variable to store the servo position 
+
 
 
  // Set digital pin numbers:
- const int servoPin1 = 9;  // The first servo pin 
- const int servoPin2 = 10; // The second servo pin 
- const int servoButtonPin = 11;  // The push button that changes the servo direstion 
- const int stepperButtonPin=2;
-
- int sPeed =25; // delay between steps (low=fast)
- int fullRev=200; //the number of steps that make a full rotation
- int distance=0;
+ const int servoPin1 = 9;  // The first servo's pin 
+ const int servoPin2 = 10; // The second servo's pin 
+ const int button1Pin = 8;  // The button that changes servo direction
+ const int button2Pin = 7; // stepper motor button to go up
+ // stepper motor button to go down 
 
 //BUTTONS 
  int button1State = 0;  // Variable for reading the pushbutton status
- int directionState = 0;  // Variable for reading direction of the servo
-
-int button2State =0;
-int OnOff=0;
-
-
-
- Servo servo1;// First servo object 
- Servo servo2; // second servo object 
- int pos = 0;  // Variable to store the servo position 
-
-
- void setup() {
- pinMode (stepperButtonPin,INPUT); // initialize the stepper pushbutton pin as an input
+ int direction1State = 0; // Variable for reading direction of the servo
+ 
+ int button2State=0;
+ int direction2State=0;
  
 
-  
-  servo1.attach(9); // attaches the servo on pin 9 to the servo object 
-  servo2.attach(10); // attaches the servo on pin 10 to the servo object 
-  pinMode(servoButtonPin, INPUT);  // initialize the servo pushbutton pin as an input
-  
+ 
+ void setup() {
+   Serial.begin(9600);           // set up Serial library at 9600 bps
+  Serial.println("Stepper test!");
+
+  AFMS.begin();  // create with the default frequency 1.6KHz
+  //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
+ 
+servo1.attach(9); // attaches the servo on pin 9 to the servo object 
+servo2.attach(10); // attaches the servo on pin 10 to the servo object 
+
 
   //LED TURNS ON
   pinMode(13, OUTPUT);
-  digitalWrite(13, HIGH);   // turns the LED on 
-   
-   
+  digitalWrite(13, HIGH); 
+
+ pinMode(button1Pin, INPUT);   // initialize the servo pushbutton pin as an input
+ pinMode(button2Pin, INPUT); 
+
  }
 
- void loop(){
+void loop(){
 
-  /*
-   * 
-   * SERVO PART 
-   * 
-   * 
-   * 
-   */
+// SERVO PART 
   
    // read the state of the pushbutton value:
-   button1State = digitalRead(servoButtonPin);
+   button1State = digitalRead(button1Pin);
 
-   if (directionState == 0){
+   if (direction1State == 0){
      //The button is pushed
      if (button1State == HIGH) {
-       directionState = 1;// The direction for the servo is clockwise
+       direction1State = 1;// The direction for the servo is clockwise
 
     // goes from 0 degrees to 180 degrees in steps of 1 degree
        for(pos = 0; pos < 180; pos=pos+1)
@@ -87,10 +78,11 @@ int OnOff=0;
        }
      }
 
-   } else if (directionState == 1) {
+   } 
+else if (direction1State == 1) {
      // The button is pushed
      if (button1State == HIGH) {
-      directionState = 0;  // The direction for the servo is anti-clockwise 
+      direction1State = 0;  // The direction for the servo is anti-clockwise 
 
        // goes from 180 degrees to 0 degrees in steps of 1 degree 
        for(pos = 180; pos>=1; pos=pos-1)
@@ -102,35 +94,41 @@ int OnOff=0;
      }
    }
 
-/*
- * STEPPER PART
- * 
- * 
- */
+//STEPPER PART
 
- myMotor->setSpeed(100);  // 10 rpm   
+   button2State = digitalRead(button2Pin);
+
+   if (direction2State == 0){
+     //The button is pushed
+     if (button2State == HIGH) {
+       direction2State = 1;// The direction for the servo is clockwise
+ myMotor->setSpeed(.1); 
+  myMotor->step(1400, FORWARD, INTERLEAVE); 
+
  
- button2State= digitalRead(stepperButtonPin);
-if (button2State == LOW) 
-{
-  OnOff=1; //should run when it is 1
-}
-
-while (OnOff ==1)
-{
-   myMotor->step(100, FORWARD, SINGLE); 
-   distance= distance + 1;
-
-   if (distance == fullRev){
-    OnOff=0;
+     }
    }
-}
-
-
-
-
-
-
 
    
- }
+else if (direction2State == 1) {
+     // The button is pushed
+     if (button2State == HIGH) {
+      direction2State = 0;  // The direction for the servo is anti-clockwise 
+ 
+ myMotor->setSpeed(10000); 
+  myMotor->step(1400, BACKWARD, INTERLEAVE); 
+myMotor->setSpeed(1); 
+  myMotor->step(100, BACKWARD, INTERLEAVE);
+  
+
+     }
+}
+
+
+ 
+     }
+
+     
+   
+
+ 
